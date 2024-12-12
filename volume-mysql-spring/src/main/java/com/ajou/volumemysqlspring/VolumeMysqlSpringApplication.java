77ceecc1e129db1ada8428c54e1e70883a16db20
InKyuHwang001 <hwanginkyu42@naver.com>
2024-12-12 22:23:13 +0900

    volume

---
+++ volume-mysql-spring/src/main/java/com/ajou/volumemysqlspring/VolumeMysqlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ajou.volumemysqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolumeMysqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolumeMysqlSpringApplication.class, args);
+	}
+
+}
